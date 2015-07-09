03bec79e8975746b29fda958cbaf26d47b2fcca9
dougen99 <dougen99@qq.com>
2015-07-09 23:39:26 +0800

    增加了ui相关处理代码

--- Assets/Script/GameManagerScript.cs
+++ Assets/Script/GameManagerScript.cs
@@ -6,16 +6,17 @@ public class GameManagerScript : MonoBehaviour
 {
     private float time;
     private int initLength = 9;
+    private UIManagerScript uiScript;
 
     public static int w = 20;
     public static int h = 11;
     public static STATE state = STATE.Start;
-    //public static List<Vector2> snake = new List<Vector2>();
+
     public static List<SnakeNode> snakeNodes = new List<SnakeNode>();
     public Vector2 direction = -Vector2.right;
     public GameObject[,] grid = new GameObject[w, h];
     public GameObject node;
-    public enum STATE { Start, Gaming, GameOver };
+    public enum STATE { Start, Gaming, GameOver, Pause };
     public static int score = 0;
     public static int level = 1;
 
@@ -24,10 +25,12 @@ public class GameManagerScript : MonoBehaviour
         public Vector2 pos;
         public Vector2 dir;
     }
-    
+
 
     void Start()
     {
+        uiScript = GetComponent<UIManagerScript>();
+
         for (int i = 0; i < initLength; ++i)
         {
             EatFood();
@@ -36,33 +39,55 @@ public class GameManagerScript : MonoBehaviour
 
     void Update()
     {
-        if (state == STATE.GameOver)
+        if (state == STATE.Gaming)
         {
-            return;
-        }
-        if (Input.GetKeyDown(KeyCode.UpArrow) && direction != -Vector2.up)
-        {
-            direction = Vector2.up;
-        }
-        else if (Input.GetKeyDown(KeyCode.DownArrow) && direction != Vector2.up)
-        {
-            direction = -Vector2.up;
-        }
-        else if (Input.GetKeyDown(KeyCode.LeftArrow) && direction != Vector2.right)
-        {
-            direction = -Vector2.right;
-        }
-        else if (Input.GetKeyDown(KeyCode.RightArrow) && direction != -Vector2.right)
-        {
-            direction = Vector2.right;
-        }
+            if (Input.GetKeyDown(KeyCode.UpArrow) && direction != -Vector2.up)
+            {
+                direction = Vector2.up;
+            }
+            else if (Input.GetKeyDown(KeyCode.DownArrow) && direction != Vector2.up)
+            {
+                direction = -Vector2.up;
+            }
+            else if (Input.GetKeyDown(KeyCode.LeftArrow) && direction != Vector2.right)
+            {
+                direction = -Vector2.right;
+            }
+            else if (Input.GetKeyDown(KeyCode.RightArrow) && direction != -Vector2.right)
+            {
+                direction = Vector2.right;
+            }
 
-        if (time > 0.2f)
-        {
-            SnakeMove(direction);
-            time = 0;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                state = STATE.Pause;
+            }
+
+            if (time > 0.2f)
+            {
+                SnakeMove(direction);
+                time = 0;
+            }
+            time += Time.deltaTime;
         }
-        time += Time.deltaTime;
+        else if (state == STATE.Pause)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                state = STATE.Gaming;
+            }
+            else if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                GameOver();
+            }
+        }
+        else if (state == STATE.GameOver)
+        {
+            if (Input.anyKeyDown)
+            {
+                ReturnMenu();
+            }
+        }
     }
 
     // 增加贪吃蛇的长度
@@ -77,19 +102,19 @@ public class GameManagerScript : MonoBehaviour
         }
         else
         {
-            lastNode.pos = snakeNodes[snakeNodes.Count - 1].pos - snakeNodes[snakeNodes.Count-1].dir;
+            lastNode.pos = snakeNodes[snakeNodes.Count - 1].pos - snakeNodes[snakeNodes.Count - 1].dir;
             lastNode.dir = snakeNodes[snakeNodes.Count - 1].dir;
             snakeNodes.Add(lastNode);
         }
 
         grid[Mathf.RoundToInt(lastNode.pos.x), Mathf.RoundToInt(lastNode.pos.y)] = (GameObject)Instantiate(node, new Vector3(lastNode.pos.x, lastNode.pos.y, 0f), Quaternion.identity);
-        
+
         // 计分
         if (snakeNodes.Count > initLength)
         {
             score = (snakeNodes.Count - initLength) * level;
 
-            if (snakeNodes.Count == w*h)
+            if (snakeNodes.Count == w * h)
             {
                 score += 100;
                 GameOver();
@@ -122,15 +147,15 @@ public class GameManagerScript : MonoBehaviour
     {
         if ((Mathf.RoundToInt(vec.x) >= w || Mathf.RoundToInt(vec.x) < 0) || (Mathf.RoundToInt(vec.y) >= h || Mathf.RoundToInt(vec.y) < 0))
         {
-            Debug.Log("碰到墙壁了"+vec);
+            Debug.Log("碰到墙壁了" + vec);
             return true;
         }
-        
-        foreach(SnakeNode node in snakeNodes)
+
+        foreach (SnakeNode node in snakeNodes)
         {
             if (vec == node.pos)
             {
-                Debug.Log("碰到身体了"+node);
+                Debug.Log("碰到身体了" + node);
                 return true;
             }
         }
@@ -148,9 +173,47 @@ public class GameManagerScript : MonoBehaviour
         }
     }
 
+    // 游戏开始函数
+    public void GameStart()
+    {
+        state = STATE.Gaming;
+        uiScript.ShowStartPanel(false);
+
+        // 重置所有数据
+        score = 0;
+        snakeNodes.Clear();
+        for (int i = 0; i < w; ++i)
+        {
+            for (int j = 0; j < h; ++j)
+            {
+                if (grid[i, j] != null)
+                {
+                    Destroy(grid[i, j]);
+                }
+            }
+        }
+
+        direction = -Vector2.right;
+
+        for (int i = 0; i < initLength; ++i)
+        {
+            EatFood();
+        }
+    }
+
     // 游戏结束函数
     public void GameOver()
     {
         state = STATE.GameOver;
+        uiScript.ShowGameOverPanel(true);
+    }
+
+    // 回到主界面
+    public void ReturnMenu()
+    {
+        state = STATE.Start;
+        uiScript.ShowGameOverPanel(false);
+        uiScript.ShowRankPanel(false);
+        uiScript.ShowStartPanel(true);
     }
 }
--- Assets/Script/UIManagerScript.cs
+++ Assets/Script/UIManagerScript.cs
@@ -5,6 +5,15 @@ using System.Collections;
 public class UIManagerScript : MonoBehaviour 
 {
     public Text score;
+    public GameObject rankPanel;
+    public Text firstscore;
+    public Text secondscore;
+    public Text thirdscore;
+
+    public GameObject startPanel;
+
+    public GameObject gameoverPanel;
+    public Text finalscore;
 
 	void Start () 
 	{
@@ -15,4 +24,23 @@ public class UIManagerScript : MonoBehaviour
 	{
         score.text = GameManagerScript.score.ToString();
 	}
+
+    public void ShowStartPanel(bool active)
+    {
+        startPanel.SetActive(active);
+    }
+
+    public void ShowRankPanel(bool active)
+    {
+        rankPanel.SetActive(active);
+    }
+
+    public void ShowGameOverPanel(bool active)
+    {
+        if (active == true)
+        {
+            finalscore.text = GameManagerScript.score.ToString();
+        }
+        gameoverPanel.SetActive(active);
+    }
 }
